bd301bb8375fb0e59ad69a22fbb4b9808616d82e
Arti <arti.iosdev@gmail.com>
2021-04-12 19:27:23 -0700

    login bug fixes

--- public/js/login.js
+++ public/js/login.js
@@ -1,68 +1,23 @@
-$(document).ready(function() {
+const loginButtonClicked = async (event) => {
+  event.preventDefault();
+  const username = document.querySelector('.input-username-login').value;
+  const password = document.querySelector('.input-password-password').value;
 
-   $( "#loginButton" ).on("click", async function(){
-     alert("in lgoin");
-
-    //event.preventDefault();
-    const username = $('.input-username-login').val();
-    const password = $('.input-password-password').val();
-    alert(username + password);
-
-    if (username && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ username, password }),
-            headers: { 'Content-Type': 'application/json' },
-          });
-                
-          alert(response);
-          return;
-
-          console.log(response);
-          if  (response.ok) {
-            //document.location.replace('/dashboard');
-          } 
-          else if (response.status == 400 || response.status == 500) {
-            alert("Incorrect email or password. Please try again!");
-          }
-    }
-   }) 
-});
-
-
-
-
-/*
-
-//const loginButtonClicked = async (event) => {
-function login() {
-    const username = $('.input-username-login').val();
-    const password = $('.input-password-password').val();
-    alert(username + password);
-
- if (username && password) {
-   const response = fetch('/login', {
-       method: 'POST',
-       body: JSON.stringify({ username, password }),
-       headers: { 'Content-Type': 'application/json' },
-     });
-           
-     console.log(response);
-     if (response.ok) {
-       alert("logged in");
-
-       //document.location.replace('/dashboard');
-     } 
-     else if (response.status == 400 || response.status == 500) {
-       alert("Incorrect email or password. Please try again!");
-     }
-}
+  if (username && password) {
+      const response = await fetch('/api/users/login', {
+          method: 'POST',
+          body: JSON.stringify({ username, password }),
+          headers: { 'Content-Type': 'application/json' },
+        });
+              
+        console.log(response);
+        if  (response.ok) {
+          document.location.replace('/Dashboard');
+        } 
+        else if (response.status == 400 || response.status == 500) {
+          alert("Incorrect email or password. Please try again!");
+        }
   }
-   
-*/
-
-    
-
-
-
+};
+document.querySelector('.form-login').addEventListener('submit', loginButtonClicked);
 
--- server.js
+++ server.js
@@ -7,12 +7,10 @@ const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const app = express();
-const sequelize = require("./config/connection");
 const { google, outlook, office365, yahoo, ics } = require("calendar-link");
 
 
-const PORT = process.env.PORT|| 3017 ;
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const PORT = process.env.PORT|| 3018 ;
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
@@ -33,7 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(process.env.PORT || 3025 , function(){
+    app.listen(process.env.PORT || 3027 , function(){
         console.log(`Express server listening on port ${this.address().port} in ${app.settings.env} mode`);
       });
 });
