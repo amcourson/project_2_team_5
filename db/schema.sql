1eda27d9a38484121f91d8f179eab4b9b52da857
Arti <arti.iosdev@gmail.com>
2021-04-14 17:34:20 -0700

    deploy to heroku

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,12 @@ DROP DATABASE IF EXISTS EventPlanner_db;
 CREATE DATABASE EventPlanner_db;
 use eventplanner_db;
 
+CREATE TABLE `type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `type_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+
 CREATE TABLE `category` (
   `id` int NOT NULL AUTO_INCREMENT,
   `categoryname` varchar(255) NOT NULL,
@@ -21,18 +27,30 @@ CREATE TABLE `comment` (
   CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 )
 
-CREATE TABLE `comment` (
+CREATE TABLE `Event` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `commenttext` varchar(255) NOT NULL,
-  `commentdate` datetime NOT NULL,
-  `event_id` int DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `startdate` datetime NOT NULL,
+  `enddate` datetime NOT NULL,
+  `virtuallink` varchar(255) DEFAULT NULL,
+  `type_id` int DEFAULT NULL,
   `user_id` int DEFAULT NULL,
+  `category_id` int DEFAULT NULL,
+  `calendar_link` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `event_id` (`event_id`),
+  KEY `type_id` (`type_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-)
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `event_ibfk_1` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`),
+  CONSTRAINT `event_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `event_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 
 CREATE TABLE `guest` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -85,6 +103,20 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) 
+
+
+CREATE TABLE `gift` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `event_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `event_id` (`event_id`),
+  CONSTRAINT `gift_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `gift_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 
